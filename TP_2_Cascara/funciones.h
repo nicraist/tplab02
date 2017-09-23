d96bc7ae4e80a4100d907cd4a262fb554431530a
nicraist <30535903+nicraist@users.noreply.github.com>
2017-09-23 00:58:03 -0300

    Update funciones.h

--- TP_2_Cascara/funciones.h
+++ TP_2_Cascara/funciones.h
@@ -25,11 +25,59 @@ int obtenerEspacioLibre(EPersona lista[]);
  * @return el indice en donde se encuentra el elemento que coincide con el parametro dni
  */
 int buscarPorDni(EPersona lista[], int dni);
+/**  
+ * carga los datos de una estructura que comprende los datos de una persona
+ * \param lista el array de la estructura que pasa como parametro
+ * \param recibe el tamaño del arraid
+ * \return datos cargados
+ *
+ */     
+
 void cargarPersonas(EPersona lista[], int TAM);
+/** 
+ * muestra los datos de todas las personas cargadas
+ * \param recibe arraid de la estructura
+ * \param tamaño del arraid
+ * \return informacion de personas cargadas
+ *
+ */     
+
 void mostrarTodasLasPersonas(EPersona[], int TAM);
+/** 
+ * Deja el estado de la estructura en 0, mediante el dni 
+ * \param Estructura de la persona
+ * \param tamaño del arraid
+ * \return Valor en 0 del indice del estado del arraid seleccionado
+ *
+ */     
+
 void borrarPersonas(EPersona[], int TAM);
+/** 
+ * Muestra individualmente dato de estructura
+ * \param Estructura
+ * \param  -
+ * \return Visualizacion actual de la persona cargada
+ *
+ */     
+
 void mostrarPersona(EPersona);
+/**  
+ * Orden alfabeticamente de los datos que han sido cargados
+ * \param estructura de arraid
+ * \param tamaño del arraid
+ * \return Datos ordenados
+ *
+ */     
+
 void ordenarPersonas(EPersona[], int TAM);
+/** 
+ * Cuenta las edades dividido en 3 rangos
+ * \param estructura EPersona
+ * \param  tamaño de arraid
+ * \return  grafico visual de asteriscos con 3 rangos seleccionados
+ *
+ */     
+
 void grafico (EPersona[]);
 
 
